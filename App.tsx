f741cc6d9501b2e71b0cdf3c094658f7d14ba0c0
eriklungulescu <eriklungulescu@outlook.com>
2022-11-09 23:04:21 -0500

    added state logic

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { QueryClient } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Homepage } from './src/pages/Homepage/Homepage';
 import { UserSessionRestaurants } from './src/pages/UserSessionRestaurants/RestaurantSession';
 
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaView style={styles.container}>
-        <UserSessionRestaurants sessionId='47c2a523-32cd-4f3a-9c9f-db02098aa2e8' />
+        <UserSessionRestaurants />
       </SafeAreaView>
     </QueryClientProvider>
   );
--- src/pages/UserSessionRestaurants/restaurantSessionMachine.ts
+++ src/pages/UserSessionRestaurants/restaurantSessionMachine.ts
@@ -16,32 +16,32 @@ export const RestaurantSessionMachine = createMachine<UserSessionRestaurantsCont
     id: 'RestaurantSessionMachine',
     initial: 'idle',
     states: {
-        idle: {
+        idle: { // Once user opens the sessions screen they are on idle until the websocket 
             on: {
                 JOIN: { target: 'joined' }
             }
         },
-        joined: {
+        joined: { // Once user connects to websocket they see screen with button to ready up
             on: {
                 SET_READY: { target: 'ready' }
             }
         },
-        ready: {
+        ready: { // Once ready they go to waiting screen
             on: {
                 START_RATING: { target: 'currentlyRating' }
             }
         },
-        currentlyRating: { 
+        currentlyRating: {  // Once everyone is ready we get restaurants and swipe through
             on: {
                 FINISH_RATING: { target: 'finishedRating' }
             } 
         },
-        finishedRating: {
+        finishedRating: { // Send preferences once done
             on: {
                 SEE_RESULTS: { target: 'results' }
             }
         },
-        results: {
+        results: { // Results page
             on: {
                 CLOSE: { 
                     actions: ['closeSession'] 
